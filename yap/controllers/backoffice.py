f5e757f087f18b42bfa2c1644900ce761169879f
tarek <devnull@localhost>
2008-09-11 22:33:54 +0000

    working on BO

--- yap/controllers/backoffice.py
+++ yap/controllers/backoffice.py
@@ -1,12 +1,47 @@
+# -*- encoding: utf-8 -*-
+# (C) Copyright 2008 Tarek Ziadé <tarek@ziade.org>
+#
+from os.path import join
+import os
 import logging
+from routes.util import redirect_to
 
 from yap.lib.base import *
+from atomisator.main.config import AtomisatorConfig
+from pylons import request
 
+root = os.path.split(os.path.dirname(__file__))[0] 
+CONFIG = join(root, 'atomisator.cfg')
 log = logging.getLogger(__name__)
 
 class BackofficeController(BaseController):
 
     def index(self):
         c.title = 'Backoffice'
+        parser = AtomisatorConfig(CONFIG)
+        c.atomisator = {}
+        c.atomisator['title'] = parser.title
+        s = [s[0] + ' ' + ' '.join(s[1]) for s in parser.sources]
+
+        c.atomisator['sources'] = '\n'.join(s)
+        c.atomisator['database'] = parser.database
+        c.atomisator['description'] = parser.description
+        c.atomisator['link'] = parser.link
+        
+        s = [s[0] + ' ' + ' '.join(s[1]) for s in parser.filters]
+        c.atomisator['filters'] = '\n'.join(s)    
+ 
+        s = [s[0] + ' ' + ' '.join(s[1]) for s in parser.enhancers]
+        c.atomisator['enhancers'] = '\n'.join(s)    
+ 
         return render('/backoffice.mako')
+
+    
+    def update(self):
+        # need to make AtomisatorConfig writeable
+        parser = AtomisatorConfig(CONFIG)
+        if request.GET.get('title', parser.title) != parser.title:
+            parser.title = request.GET.get('title')
+        parser.write(open(CONFIG, 'w'))
+        redirect_to(action='index')
 
--- yap/templates/backoffice.mako
+++ yap/templates/backoffice.mako
@@ -1,9 +1,45 @@
 <%inherit file="/base.mako" />
 <h1>Atomisator configuration</h1>
 
-XXX put here a form for configuration
-(atomisator .cfg file)
 
+${h.form(h.url(action='update'), method='get')}
+<table>
+  <tr>
+    <td>Title</td>
+    <td>${h.text_field('title', value=c.atomisator['title'])}</td>
+  </tr>
+  <tr>
+    <td>Sources</td>
+    <td>${h.text_area('sources', c.atomisator['sources'], size="100x18")}</td>
+  </tr>
+  <tr>
+    <td>Database</td>
+    <td>${h.text_field('database', value=c.atomisator['database'], 
+                       size=50)}</td>
+  </tr>
+  <tr>
+    <td>Description</td>
+    <td>${h.text_field('description', value=c.atomisator['description'], size=50)}</td>
+  </tr>
+  <tr>
+    <td>Link</td>
+    <td>${h.text_field('link', value=c.atomisator['link'], size=50)}</td>
+  </tr>
+  <tr>
+    <td>Filters</td>
+    <td>${h.text_area('filters', c.atomisator['filters'], size="50x10")}</td>
+  </tr>
+  <tr>
+    <td>Enhancers</td>
+    <td>${h.text_area('enhancers', c.atomisator['enhancers'], size="50x10")}</td>
+  </tr>
+  <tr>
+   <td colspan="2">${h.submit('Submit')}</td>
+  </tr>
+</table>
+
+${h.end_form()}
+ 
 <div>
  <a href="/">Back To Front</a>
 </div>
